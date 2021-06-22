dc44e370a3b255a7e50b2c2c4f1cae78b3d53b78
Jesus Sabroso <gs@MacBook-Pro-de-gs.local>
2021-06-22 10:36:20 +0200

    test deploy build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import YouTube from "react-youtube";
+import "./App.css";
+
+const VideoWidget = ({ videoId, videoAutoplay = false }) => {
+  const opts = {
+    playerVars: {
+      autoplay: 1,
+      muted: 1,
+    },
+  };
+
+  return <YouTube videoId={videoId} opts={opts} />;
+};
 
 function App() {
   return (
@@ -18,6 +30,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <VideoWidget videoId="vkPxNwV3izw" videoAutoplay="true"></VideoWidget>
     </div>
   );
 }
